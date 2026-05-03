f25cf96ee74dd129c563a3988283ab2d7a7ebdfc
Frithjof Siebecke <fsiebecke@gmail.com>
2026-05-03 12:38:37 +0200

    added zigbee2mqtt converter

---
+++ main/redfivedesigns.js
@@ -0,0 +1,53 @@
+import * as m from 'zigbee-herdsman-converters/lib/modernExtend';
+import * as exposes from 'zigbee-herdsman-converters/lib/exposes';
+
+const e = exposes.presets;
+
+/* ========= WALLSWITCH (unverändert) ========= */
+const wallswitch = {
+    zigbeeModel: ['4-gang-wallswitch'],
+    model: 'Four Gang Wallswitch (TI)',
+    vendor: 'redfivedesigns',
+    description: 'Custom 4-gang switch',
+    extend: [
+        m.deviceEndpoints({"endpoints": {"1": 1, "2": 2, "3": 3, "4": 4}}),
+        m.commandsOnOff({"endpointNames": ["1", "2", "3", "4"]}),
+    ],
+    fromZigbee: [{
+        cluster: 'genBasic',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.physicalEnvironment !== undefined) {
+                const modes = {0: 'brightness', 1: 'color_temp', 2: 'hue'};
+                result.control_mode = modes[msg.data.physicalEnvironment];
+            }
+            if (msg.data.locationDescription !== undefined) {
+                const actions = {2: 'double', 4: 'hold', 5: 'release'};
+                result.action = actions[msg.data.locationDescription];
+            }
+            return result;
+        },
+    }],
+    exposes: [
+        e.enum('control_mode', 1, ['brightness', 'color_temp', 'hue']),
+        e.enum('action', 1, ['single', 'double', 'hold', 'release', 'toggle_1']),
+    ],
+};
+
+/* ========= LAMPE ========= */
+const colorlight = {
+    zigbeeModel: ['rgbww-colorlight'],
+    model: 'RGBWW Color Light',
+    vendor: 'redfivedesigns',
+    description: 'ESP Zigbee RGBWW light',
+
+    extend: [
+        m.light({
+            color: {xy: true},
+            brightness: true,
+        }),
+    ],
+};
+
+export default [wallswitch, colorlight];
